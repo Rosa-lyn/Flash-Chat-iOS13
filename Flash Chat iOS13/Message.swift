82ca24f21c3757f2e7db611a45a1bf1a5086bd4c
Rosalyn Land <roz.s.land@gmail.com>
2022-11-22 10:41:20 +0000

    Building up UITableView

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Rosalyn Land on 22/11/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // 1@2.com
+    let body: String
+}
